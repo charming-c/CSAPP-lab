e0a5debf7be251cd2a8154978197ebe3b9ffc9aa
charming-c <822170415@qq.com>
2023-12-03 07:53:33 +0000

    malloc lab

--- malloclab-handout/mm-IFC.c
+++ malloclab-handout/mm-IFC.c
@@ -71,14 +71,8 @@ team_t team = {
 #define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp))) 
 #define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE((char *)(bp) - (DSIZE))) 
 
-// 获取空闲链表节点的上一个节点
-#define NEXT_FREE_BLK(bp) ((char *)(bp) + WSIZE)
-#define PREV_FREE_BLK(bp) ((char *)(bp) + 0)
-#define PUT_ADDR(bp, val) (*((void **)(bp)) = val)
-#define GET_ADDR(bp) (*((void **)(bp)))
 
 static void *heap_listp = 0;
-static void *free_listp = 0;
 static void *extend_heap(size_t words);
 static void place(void *bp, size_t asize);
 static void *find_fit(size_t asize);
@@ -86,11 +80,6 @@ static void *coalesce(void *bp);
 static void printblock(void *bp); 
 static void checkheap(int verbose);
 static void checkblock(void *bp);
-static void checklist();
-static void delete_node(void *bp);
-static void insert(void *bp);
-
-
 
 /* 
  * mm_init - initialize the malloc package.
@@ -105,7 +94,7 @@ int mm_init(void)
     PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1)); /* Prologue header */ 
     PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1)); /* Prologue footer */ 
     PUT(heap_listp + (3 * WSIZE), PACK(0, 1));     /* Epilogue header */
-    heap_listp += (2 * WSIZE);   
+    heap_listp += (2 * WSIZE);    
 
     if((void *)extend_heap(CHUNKSIZE/WSIZE) == NULL)
         return -1;
@@ -122,7 +111,7 @@ static void *extend_heap(size_t words) {
     
     PUT(HDRP(bp), PACK(size, 0));
     PUT(FTRP(bp), PACK(size, 0));
-    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));       /* 处理尾块 */
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
 
     return coalesce((void *)bp);
 }
@@ -146,7 +135,6 @@ void *mm_malloc(size_t size)
         asize = DSIZE * ((size + (DSIZE) + (DSIZE - 1)) / DSIZE);
 
     if((bp = find_fit(asize)) != NULL) {
-        // checkheap(1);
         place(bp, asize);
         return bp;
     }
@@ -154,35 +142,25 @@ void *mm_malloc(size_t size)
     extendsize = MAX(asize, CHUNKSIZE);
     if((bp = extend_heap(extendsize/WSIZE)) == NULL)
         return NULL;
-    // checkheap(1);
     place(bp, asize);
     // checkheap(1);
     return bp;
 }
 
 static void *find_fit(size_t size) {
-    // void *p = heap_listp;
-    // while(GET_SIZE(HDRP(p)) != 0) {
-    //     if(GET_ALLOC(HDRP(p)) == 1) {
-    //         p = NEXT_BLKP(p);
-    //     }
-    //     else {
-    //         if(GET_SIZE(HDRP(p)) >= size) {
-    //             return (char *)p;
-    //         }
-    //         else {
-    //             p = NEXT_BLKP(p);
-    //         }
-    //     }
-    // }
-    // return NULL;
-    void *p = free_listp;
-    if(p == 0) return NULL;
-    while(p != heap_listp) {
-        // checklist();
-        if(GET_SIZE(HDRP(p)) < size)
-            p = GET_ADDR(NEXT_FREE_BLK(p));
-        else return p;
+    void *p = heap_listp;
+    while(GET_SIZE(HDRP(p)) != 0) {
+        if(GET_ALLOC(HDRP(p)) == 1) {
+            p = NEXT_BLKP(p);
+        }
+        else {
+            if(GET_SIZE(HDRP(p)) >= size) {
+                return (char *)p;
+            }
+            else {
+                p = NEXT_BLKP(p);
+            }
+        }
     }
     return NULL;
 }
@@ -190,20 +168,21 @@ static void *find_fit(size_t size) {
 static void place(void* bp, size_t size) {
     size_t old_size = GET_SIZE(HDRP(bp));
     size_t extra = old_size - size;
-    delete_node(bp);
-
+    
     if(extra >= (2 * DSIZE)) {
         PUT(HDRP(bp), PACK(size, 1));
         PUT(FTRP(bp), PACK(size, 1));
         PUT(HDRP(NEXT_BLKP(bp)), PACK(extra, 0));
         PUT(FTRP(NEXT_BLKP(bp)), PACK(extra, 0));
-        insert(NEXT_BLKP(bp));
     }
     else {
         PUT(HDRP(bp), PACK(old_size, 1));
         PUT(FTRP(bp), PACK(old_size, 1));
     }
 
+    if((GET_SIZE(HDRP(NEXT_BLKP(bp))) != 0) && (GET_ALLOC(HDRP(NEXT_BLKP(bp))) == 0)) {
+        coalesce(NEXT_BLKP(bp));
+    }
 }
 
 /*
@@ -224,34 +203,26 @@ static void *coalesce(void *bp) {
     size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
     size_t size = GET_SIZE(HDRP(bp));
     if(prev_alloc && next_alloc) {
-        insert(bp);
         return bp;
     }
-    else if(prev_alloc && (!next_alloc)) {
-        delete_node(NEXT_BLKP(bp));
+    else if(prev_alloc && !next_alloc) {
         size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
         PUT(HDRP(bp), PACK(size, 0));
         PUT(FTRP(bp), PACK(size, 0));
-        insert(bp);
     }
-    else if((!prev_alloc) && next_alloc) {
-        delete_node(PREV_BLKP(bp));
+    else if(!prev_alloc && next_alloc) {
         size += GET_SIZE(FTRP(PREV_BLKP(bp)));
         PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
         PUT(FTRP(bp), PACK(size, 0));
 
         bp = PREV_BLKP(bp);
-        insert(bp);
     }
     else {
-        delete_node(NEXT_BLKP(bp));
-        delete_node(PREV_BLKP(bp));
         size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
         PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
         PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
 
         bp = PREV_BLKP(bp);
-        insert(bp);
     }
     return bp;
 }
@@ -260,6 +231,7 @@ static void printblock(void *bp)
 {
     size_t hsize, halloc, fsize, falloc;
 
+    checkheap(0);
     hsize = GET_SIZE(HDRP(bp));
     halloc = GET_ALLOC(HDRP(bp));  
     fsize = GET_SIZE(FTRP(bp));
@@ -293,7 +265,7 @@ void checkheap(int verbose)
     if (verbose)
         printf("Heap (%p):\n", heap_listp);
 
-    if ((GET_SIZE(HDRP(heap_listp)) != 2 * WSIZE) || !GET_ALLOC(HDRP(heap_listp)))
+    if ((GET_SIZE(HDRP(heap_listp)) != DSIZE) || !GET_ALLOC(HDRP(heap_listp)))
         printf("Bad prologue header\n");
     checkblock(heap_listp);
 
@@ -349,77 +321,7 @@ void *mm_realloc(void *ptr, size_t size)
     return newptr;
 }
 
-static void checklist() {
-    void *p = free_listp;
-    int i = 0;
-    puts("linkedlist:");
-    if(p == 0) return;
-    void *q = GET_ADDR(NEXT_FREE_BLK(p));
-    printf("%p\n", q);
-    while(GET_ADDR(NEXT_FREE_BLK(p)) != heap_listp) {
-        printf("node%d: %p\nprev_node: %p\nnext_node: %p\n", i++, p, GET_ADDR(PREV_FREE_BLK(p)), GET_ADDR(NEXT_FREE_BLK(p)));
-        p = GET_ADDR(NEXT_FREE_BLK(p));
-    }
-    printf("node%d: %p\nprev_node: %p\nnext_node: %p\n", i, p, GET_ADDR(PREV_FREE_BLK(p)), GET_ADDR(NEXT_FREE_BLK(p)));
-}
 
-/**
- * 删除空闲块
- */
-static void delete_node(void *bp) {
-    // checkheap(1);
-    char *prev = GET_ADDR(PREV_FREE_BLK(bp));
-    char *next = GET_ADDR(NEXT_FREE_BLK(bp));
-    /* 四种情况 */
-    /* 1. 既是头节点又是尾节点 */
-    /* 2. 只是头节点 */
-    /* 3. 只是尾节点 */
-    /* 4. 既不是头，也不是尾节点 */
-    if(prev == heap_listp && next == heap_listp) {
-        free_listp = 0;
-    }
-    else if(prev == heap_listp && next != heap_listp) {
-        PUT_ADDR(PREV_FREE_BLK(next), heap_listp);
-        free_listp = next;
-    }
-    else if(prev != heap_listp && next == heap_listp) {
-        PUT_ADDR(NEXT_FREE_BLK(prev), heap_listp);
-    }
-    else {
-        PUT_ADDR(PREV_FREE_BLK(next), prev);
-        PUT_ADDR(NEXT_FREE_BLK(prev), next);
-
-    }
-    PUT_ADDR(PREV_FREE_BLK(bp), 0);
-    PUT_ADDR(NEXT_BLKP(bp), 0);
-    // checklist();
-}
-
-
-/**
- * 头插法向当前空闲链表插入空闲块
- */
-static void insert(void *bp) {
-    // checkheap(1);
-    /* 链表不为空时 */
-    if(free_listp != 0) {
-        PUT_ADDR(PREV_FREE_BLK(free_listp), bp);
-        PUT_ADDR(PREV_FREE_BLK(bp), heap_listp);
-        PUT_ADDR(NEXT_FREE_BLK(bp), free_listp);
-        free_listp = bp;
-    }
-    /* 为空 */
-    else {
-        PUT_ADDR(PREV_FREE_BLK(bp), heap_listp);
-        PUT_ADDR(NEXT_FREE_BLK(bp), heap_listp);
-        free_listp = bp;
-    }
-    if(GET_ADDR(PREV_FREE_BLK(bp)) == bp)
-        PUT_ADDR(PREV_FREE_BLK(bp), heap_listp);
-    if(GET_ADDR(NEXT_FREE_BLK(bp)) == bp)
-        PUT_ADDR(NEXT_FREE_BLK(bp), heap_listp);
-    // checklist();
-}
 
 
 
